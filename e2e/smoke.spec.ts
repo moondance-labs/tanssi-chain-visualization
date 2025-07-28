a1f9eacf3c44d8cd5025814f610ade4aa7d4f455
Evgeny Svirsky <e.svirsky@tut.by>
2025-07-28 12:34:54 +0200

    - Updated title for health check test

--- e2e/smoke.spec.ts
+++ e2e/smoke.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('website contains required texts', async ({ page }) => {
   await page.goto('https://moondance-labs.github.io/tanssi-chain-visualization/');
 
-  for (const text of ['Tanssi chains visualization', 'Stagelight', 'Moonlight', 'Tanssi', 'Dancebox', 'Flashbox']) {
+  for (const text of ['Chains dashboard', 'Stagelight', 'Moonlight', 'Tanssi', 'Dancebox', 'Flashbox']) {
     await expect(page.locator(`text=${text}`).first()).toBeVisible();
   }
 });
